c2dbd6998a6b29261474e9c422aafdc2f53b249a
Greekkgod <gaurpradhumay@gmail.com>
2026-09-24 12:10:32 +0530

    test: remove temporary test_hf_api.py script

--- test_hf_api.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import HfApi
-
-api = HfApi()
-models = list(api.list_models(search="llama", pipeline_tag="text-generation", limit=3, expand=["safetensors"]))
-print("With expand=safetensors:")
-for m in models:
-    print(getattr(m, "safetensors", None))
